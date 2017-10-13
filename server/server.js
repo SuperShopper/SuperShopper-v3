d6f5687d7839abd4eb2f3a75ca7d95be5da8c41e
Gordon <godronus@yahoo.com>
2017-10-13 15:35:25 -0700

    Server ready to test

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const itemController = require('./controllers/itemController');
+const itemDetailsController = require('./controllers/itemDetailsController');
+const metaController = require('./controllers/metaController');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,9 +13,13 @@ app.use(bodyParser.json());
 
 app.get('/', itemController.getTopItems);
 
-app.post('/findItems', metaController.searchItems
+app.post('/findItems', metaController.searchItems,
                        itemController.getAllItemsFromIds);
 
 app.post('/getItemById', itemController.getItemById);
+
+app.post('/addNewItem', itemController.addNewItem,
+                        itemDetailsController.addNewItemDetail,
+                        metaController.addNewItemsMetaWords);
 
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
